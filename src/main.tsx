42c53e9ba684e1c882727760ea972746d246a01c
lunghr <myilanka8@gmail.com>
2025-01-12 20:35:59 +0300

    [-] Remove placeholder files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'virtual:uno.css'
-import './index.css'
+import './index.scss'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
