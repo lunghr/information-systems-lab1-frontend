0141104dff7077feeafb5bab2d168320659260e3
lunghr <myilanka8@gmail.com>
2025-01-13 01:51:02 +0300

    [+] Router implementation

---
+++ src/pages/Router.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Auth } from "./Auth";
+import { Register } from "./Register";
+import { useAuthStore } from "../context/authContext";
+import { useEffect } from "react";
+import { observer } from "mobx-react-lite";
+
+// Vite base URL
+const basePath = import.meta.env.BASE_URL;
+
+const RouterComponent = observer(() => {
+  const authContext = useAuthStore();
+
+  useEffect(() => {
+    console.log(authContext);
+  });
+
+  return (
+    <BrowserRouter basename={basePath}>
+      <Routes>
+        {!authContext.isAuth ? (
+          <>
+            <Route path="auth/login" element={<Auth />} />
+            <Route path="auth/register" element={<Register />} />
+          </>
+        ) : (
+          <>Dibil</>
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
+});
+
+export default RouterComponent;
