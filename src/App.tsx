2ca4fde4bfa06f4d83fe8c3e310fa16013969782
lunghr <myilanka8@gmail.com>
2025-01-12 20:56:31 +0300

    [+] Router placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <></>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Vite base URL
+const basePath = import.meta.env.BASE_URL;
+
+const App = () => {
+  return (
+    <BrowserRouter basename={basePath}>
+      <Routes>
+        <Route path="/" element={<h1>Hello, world!</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
