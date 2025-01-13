a193dda0b1d4047d99b559d2e31ac1ba99d4c31d
lunghr <myilanka8@gmail.com>
2025-01-13 06:03:47 +0300

    [+] App configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, CssVarsProvider } from "@mui/joy";
+import authStore from "./store/auth";
+import { AuthContext } from "./context/authContext";
+import Router from "./pages/Router";
+import Controls from "./components/Controls";
+import { BrowserRouter } from "react-router-dom";
 
 // Vite base URL
 const basePath = import.meta.env.BASE_URL;
 
 const App = () => {
   return (
-    <BrowserRouter basename={basePath}>
-      <Routes>
-        <Route path="/" element={<h1>Hello, world!</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={authStore}>
+      <BrowserRouter basename={basePath}>
+        <CssVarsProvider defaultMode="system">
+          <CssBaseline />
+          <Router />
+          <Controls />
+        </CssVarsProvider>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };
 
