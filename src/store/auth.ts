0a5ada9f1fc390b7da1fed8c442b4b17133f829d
lunghr <myilanka8@gmail.com>
2025-01-13 03:37:18 +0300

    [+] Save credential to local storage

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,5 +1,7 @@
 import { makeAutoObservable } from "mobx";
 
+const AUTH_STORE_ID = "authStore";
+
 class AuthStore {
   isAuthenticated: boolean = false;
   username?: string;
@@ -7,18 +9,31 @@ class AuthStore {
 
   constructor() {
     makeAutoObservable(this);
+    this.loadFromStorage()
+  }
+
+  async loadFromStorage() {
+    if (localStorage.getItem(AUTH_STORE_ID)) {
+      const token = localStorage.getItem(AUTH_STORE_ID)!;
+
+      this.isAuthenticated = true;
+      this.token = token;
+      this.username = "TEST";
+    }
   }
 
   setAuth(username: string, token: string) {
     this.isAuthenticated = true;
     this.username = username;
     this.token = token;
+    localStorage.setItem(AUTH_STORE_ID, token);
   }
 
   logout() {
     this.isAuthenticated = false;
     this.username = undefined;
     this.token = undefined;
+    localStorage.removeItem(AUTH_STORE_ID);
   }
 
   get isAuth() {
