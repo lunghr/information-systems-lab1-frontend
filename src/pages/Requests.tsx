2b019841248530aabff57b9e51cdf8850ab34950
lunghr <myilanka8@gmail.com>
2025-01-13 06:03:18 +0300

    [+] Pages placeholders & routing

---
+++ src/pages/Requests.tsx
@@ -0,0 +1,9 @@
+const RequestsPage = () => {
+  return (
+    <div>
+      <h1>Requests Page</h1>
+    </div>
+  );
+};
+
+export default RequestsPage;
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Auth from "./Auth";
 import Register from "./Register";
 import { useAuthStore } from "../context/authContext";
 import { useEffect } from "react";
 import { observer } from "mobx-react-lite";
-
-// Vite base URL
-const basePath = import.meta.env.BASE_URL;
+import NotFound from "./NotFound";
+import MainPage from "./Main";
+import ProfilePage from "./Profile";
+import RequestsPage from "./Requests";
+import UsersPage from "./Users";
 
 const RouterComponent = observer(() => {
   const authContext = useAuthStore();
@@ -16,18 +18,24 @@ const RouterComponent = observer(() => {
   });
 
   return (
-    <BrowserRouter basename={basePath}>
-      <Routes>
-        {!authContext.isAuth ? (
-          <>
-            <Route path="auth/login" element={<Auth />} />
-            <Route path="auth/register" element={<Register />} />
-          </>
-        ) : (
-          <>Dibil</>
-        )}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {!authContext.isAuth ? (
+        <>
+          <Route path="auth/login" element={<Auth />} />
+          <Route path="auth/register" element={<Register />} />
+          <Route path="*" element={<Auth />} />
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/admin/users" element={<UsersPage />} />
+          <Route path="/admin/requests" element={<RequestsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<NotFound />} />
+        </>
+      )}
+    </Routes>
   );
 });
 
